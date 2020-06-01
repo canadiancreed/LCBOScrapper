7ae77c83831f5e17cfef91774d6a4f13d283f1e2
Chris Reed <creedis@gmail.com>
2020-06-01 16:55:12 -0400

    Added ability to spawn mulitple processes via bash to divide workload

---
+++ src/code/Initialize.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+python Application.py WINE &
+python Application.py RED_WINE &
+python Application.py WHITE_WINE &
+python Application.py OTHER_WINE &
+python Application.py SPIRITS &
+python Application.py OTHER_SPIRITS &
+python Application.py WHISKEY &
+python Application.py OTHER_WHISKEY &
+python Application.py BEER_CIDER &
+python Application.py LAGER &
+python Application.py ALE_HYBRID &
+python Application.py OTHER &
